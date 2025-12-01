9e3ec51924e00cec59cc394eed4ce8f83e5299f0
ravveni <ravveni420@gmail.com>
2025-12-01 00:09:19 -0500

    changed regex-input to input, consolidated css styles, added color index reset

--- src/renderer.js
+++ src/renderer.js
@@ -2,11 +2,51 @@ window.addEventListener('load', updateResults);
 document.getElementById('regex-input').addEventListener('input', updateResults);
 document.getElementById('test-string').addEventListener('input', updateResults);
 
+let currentColorIndex = 0;
+
+const colors = [
+  `hsl(351, 74%, 73%)`,
+  `hsl(21, 86%, 73%)`,
+  `hsl(40, 70%, 78%)`,
+  `hsl(105, 48%, 72%)`,
+  `hsl(189, 59%, 73%)`,
+  `hsl(220, 83%, 75%)`,
+  `hsl(267, 83%, 80%)`,
+]
+
+function createResultBox(text, hasBackground) {
+  const span = document.createElement('span');
+
+  span.style.padding = '5px';
+  span.style.marginRight = '10px';
+  span.style.display = 'inline-block';
+  span.style.borderRadius = '5px';
+
+  span.style.fontSize = '14px';
+  span.style.color = `hsl(227, 68%, 88%)`;
+  span.textContent = text;
+
+  if (hasBackground) {
+    span.style.marginBottom = '5px';
+    span.style.marginTop = '5px';
+    span.style.backgroundColor = colors[currentColorIndex];
+    span.style.color = `hsl(236, 23%, 12%)`;
+  }
+
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+  document.getElementById('results').appendChild(span);
+}
+
+function reset() {
+  document.getElementById('results').innerHTML = '';
+  currentColorIndex = 0;
+}
+
 function updateResults() {
   const regexInput = document.getElementById('regex-input').value.trim();
   const testString = document.getElementById('test-string').value;
 
-  document.getElementById('results').innerHTML = '';
+  reset();
 
   if (!regexInput && !testString) {
     createResultBox(' ', false);
@@ -38,39 +78,4 @@ function updateResults() {
   catch (e) {
     createResultBox(e.message, false);
   }
-}
-
-const colors = [
-  `hsl(351, 74%, 73%)`,
-  `hsl(21, 86%, 73%)`,
-  `hsl(40, 70%, 78%)`,
-  `hsl(105, 48%, 72%)`,
-  `hsl(189, 59%, 73%)`,
-  `hsl(220, 83%, 75%)`,
-  `hsl(267, 83%, 80%)`,
-]
-
-let currentIndex = 0;
-
-function createResultBox(text, hasBackground) {
-  const span = document.createElement('span');
-
-  span.style.padding = '5px';
-  span.style.marginRight = '10px';
-  span.style.display = 'inline-block';
-  span.style.borderRadius = '4px';
-
-  span.style.fontSize = '14px';
-  span.style.color = `hsl(227, 68%, 88%)`;
-  span.textContent = text;
-
-  if (hasBackground) {
-    span.style.marginBottom = '5px';
-    span.style.marginTop = '5px';
-    span.style.backgroundColor = colors[currentIndex];
-    span.style.color = `hsl(236, 23%, 12%)`;
-  }
-
-  currentIndex = (currentIndex + 1) % colors.length;
-  document.getElementById('results').appendChild(span);
 }
